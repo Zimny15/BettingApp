8f3de9e275d4a3a128158820d00d641fa5176d55
Jakub Zima <jzima@softguard.pl>
2025-05-30 00:39:51 +0200

    Migrations

--- BookmakerApp/BookmakerApp/Data/ApplicationDbContext.cs
+++ BookmakerApp/BookmakerApp/Data/ApplicationDbContext.cs
@@ -4,4 +4,15 @@ using Microsoft.EntityFrameworkCore;
 namespace BookmakerApp.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+
+        modelBuilder.Entity<ApplicationUser>()
+            .Property(u => u.Balance)
+            .HasColumnType("decimal(18,2)");
+
+    }
 }
--- BookmakerApp/BookmakerApp/Data/ApplicationUser.cs
+++ BookmakerApp/BookmakerApp/Data/ApplicationUser.cs
@@ -4,5 +4,12 @@ namespace BookmakerApp.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    public string FirstName { get; set; } 
+    public string LastName { get; set; }
+    public decimal Balance { get; set; } = 0.00M;
+    public DateTime DateOfBirth { get; set; }
+    public string Gender { get; set; }
+    public string Country { get; set; }
+    public DateTime? LastLogin { get; set; }
 }
 
